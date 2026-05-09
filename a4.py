b648b69473fc4c8e16540ff8e89ce1cc38e12874
PrachiJunwaleCodingal <prachi.junwale@gmail.com>
2026-05-09 13:47:53 +0530

    Add files via upload

---
+++ a4.py
@@ -0,0 +1,9 @@
+import datetime  
+current_time = datetime.datetime.now()  
+      
+print("Time now at greenwich meridian is : ", end = "")   
+print(current_time)
+
+# print calendar of year 2026
+import calendar
+print("\n", calendar.calendar(2026))
